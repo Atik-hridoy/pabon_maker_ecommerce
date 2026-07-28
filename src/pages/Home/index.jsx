886a3614236b2fa8616a4ac19fab8ec444c5274b
Atik-hridoy <atikahmedridoy@gmail.com>
2026-07-29 05:08:25 +0600

    fix: remove BestSellers section from Home view

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import MainLayout from '../../components/layout/MainLayout';
 import Hero from './components/Hero';
 import Features from './components/Features';
 import CategoriesGrid from './components/CategoriesGrid';
-import BestSellers from './components/BestSellers';
+
 import PromoBanner from './components/PromoBanner';
 import Metrics from './components/Metrics';
 import Testimonials from './components/Testimonials';
@@ -14,7 +14,7 @@ export default function Homepage() {
       <Hero />
       <Features />
       <CategoriesGrid />
-      <BestSellers />
+
       <PromoBanner />
       <Metrics />
       <Testimonials />
